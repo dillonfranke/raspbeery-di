48dce06c68113cb3859901daa249d156284bf854
Dillon Reece Franke <dfranke@stanford.edu>
2020-06-12 18:58:09 -0700

    POC for edge detection on the die table

---
+++ img_analyze.py
@@ -0,0 +1,14 @@
+from skimage import filters
+from skimage import color
+from skimage import exposure
+from skimage import io
+from skimage import data
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+die_table = io.imread('imgs/IMG_2834.jpg')
+
+# edges = filters.sobel(die_table)
+
+io.imsave('test2.png', edges)
