5f0d4180fafe367fd3e1b97963437dabfd85e895
Ryan Foulke <rfoulke@stanford.edu>
2020-06-14 16:56:25 -0700

    working die location algo[exclamation point]

--- img_analyze.py
+++ img_analyze.py
@@ -9,26 +9,58 @@ from collections import Counter
 import math
 
 
-def find_x(yellow_rows):
+def find_color(die_table):
+    yellow_rows = []
+    yellow_cols = []
+    for x in range(die_table.shape[0]):
+        for y in range(die_table.shape[1]):
+            if die_table[x][y][1] > 180:
+                yellow_rows.append(x)
+                yellow_cols.append(y)
+    return yellow_rows, yellow_cols
+
+
+def on_table_rows(x):
     edges = io.imread('imgs/edge_detect.png')
     row_color = []
+    for i in range(edges.shape[1]):
+        row_color.append(np.sum(edges[x][i])-255)
+    return np.mean(row_color) > 50
+
+
+def on_table_cols(y):
+    edges = io.imread('imgs/edge_detect.png')
+    col_color = []
     for i in range(edges.shape[0]):
-        row_color.append(np.sum(edges[0][i])-255)
-    print()
-    x_val = 0
-    for x in row_counts.keys():
-        count = row_counts[x]
+        col_color.append(np.sum(edges[i][y])-255)
+    return np.mean(col_color) > 50
 
 
-die_table = io.imread('imgs/IMG_2834.jpg')
+def find_row(yellow_rows):
+    row_val = 0
+    largest_count = 0
+    row_counts = Counter(yellow_rows)
+    for row in row_counts.keys():
+        if(on_table_rows(row) and row_counts[row] > largest_count):
+            row_val = row
+            largest_count = row_counts[row]
+    return row_val
 
-num = 0
-yellow_cols = []
-yellow_rows = []
-for x in range(die_table.shape[0]):
-    for y in range(die_table.shape[1]):
-        if die_table[x][y][1] > 180:
-            yellow_rows.append(x)
-            yellow_cols.append(y)
 
-find_x(yellow_rows)
+def find_col(yellow_cols):
+    col_val = 0
+    largest_count = 0
+    col_counts = Counter(yellow_cols)
+    for col in col_counts.keys():
+        if(on_table_cols(col) and col_counts[col] > largest_count):
+            col_val = col
+            largest_count = col_counts[col]
+    return col_val
+
+
+if __name__ == '__main__':
+    die_table = io.imread('imgs/IMG_2834.jpg')
+    yellow_rows, yellow_cols = find_color(die_table)
+    row = find_row(yellow_rows)
+    col = find_col(yellow_cols)
+    print(col, row)
